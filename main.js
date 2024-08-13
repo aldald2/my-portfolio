f89b4da4c0d4c0e5c5ba3afda1b4a34fb18163f6
𝒞𝓁𝒶𝒾𝓇𝑒 𝐿𝑒𝑒 <claire@s-MacBook-Pro.local>
2024-08-13 00:11:43 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true     
+})
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
